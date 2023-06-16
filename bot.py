ed8ecc1aa8b7f7916d7374285f1e1ad349c40d2f
Flash-kir <flash-kir@yandex.ru>
2023-06-16 10:53:40 +0300

    add DialogFlow learning script.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from telegram import ForceReply, Update
 from telegram.ext import (
@@ -29,6 +30,34 @@ def bot_answer(update: Update, context: CallbackContext):
     update.message.reply_text(answer_text)
 
 
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
 def detect_intent_texts(project_id, session_id, texts, language_code='ru'):
     session_client = dialogflow.SessionsClient()
 
@@ -47,19 +76,41 @@ def detect_intent_texts(project_id, session_id, texts, language_code='ru'):
         return response.query_result.fulfillment_text
 
 
+def load_questions(path):
+    with open(path, "r") as my_file:
+        questions_json = my_file.read()
+    
+    questions = json.loads(questions_json)
+
+    for intent_name in questions.keys():
+        intent = questions[intent_name]
+        create_intent(
+            os.environ.get('PROJECT_ID'),
+            intent_name,
+            intent['questions'],
+            [intent['answer']],
+        )
+
+
 def main():
     load_dotenv()
     telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
     api_key_string = os.environ.get('API_KEY')
     quota_project_id = os.environ.get('PROJECT_ID')
 
+#    load_questions('questions.json')
 #    print(create_api_key(quota_project_id, 'my_key'))
 
     updater = Updater(telegram_bot_token)
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, bot_answer))
+    dispatcher.add_handler(
+        MessageHandler(
+            Filters.text & ~Filters.command,
+            bot_answer
+        )
+    )
 
     updater.start_polling()
     updater.idle()
