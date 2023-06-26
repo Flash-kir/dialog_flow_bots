45cb12bce2f6fd814d484ac7c7aefcc3fd4e0ebe
Flash-kir <flash-kir@yandex.ru>
2023-06-26 12:29:51 +0300

    delete for cycle and duplicate import

--- dialog_flow_utils.py
+++ dialog_flow_utils.py
@@ -2,9 +2,6 @@ from google.cloud import dialogflow
 
 
 def create_intent(project_id, display_name, training_phrases_parts, message_texts):
-    """Create an intent of the given intent type."""
-    from google.cloud import dialogflow
-
     intents_client = dialogflow.IntentsClient()
 
     parent = dialogflow.AgentsClient.agent_path(project_id)
@@ -35,18 +32,17 @@ def detect_intent_texts(project_id, session_id, texts, language_code='ru'):
 
     session = session_client.session_path(project_id, session_id)
 
-    for text in texts:
-        text_input = dialogflow.TextInput(
-            text=text,
-            language_code=language_code
-        )
+    text_input = dialogflow.TextInput(
+        text=texts[0],
+        language_code=language_code
+    )
 
-        query_input = dialogflow.QueryInput(text=text_input)
+    query_input = dialogflow.QueryInput(text=text_input)
 
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-        return (
-            response.query_result.fulfillment_text,
-            not response.query_result.intent.is_fallback
-        )
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return (
+        response.query_result.fulfillment_text,
+        not response.query_result.intent.is_fallback
+    )
